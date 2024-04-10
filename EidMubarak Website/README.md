70bb6ae9373f318c1ffe2b002ac0ccc2e893e9e1
Hassan Yaseen Siddiqui <hassanyaseen615@gmail.com>
2024-04-10 09:49:49 +0500

    1st

---
+++ EidMubarak Website/README.md
@@ -0,0 +1,2 @@
+# Eid Mubarak Static Web Template
+A simple web template (html + css + js ) script to congratulate someone. In this sample script is congratulate about Eid Mubarak.
---
+++ EidMubarak Website/js/animate.js
@@ -0,0 +1,40 @@
+function animate(typewriter) {
+    sequence = [
+        {
+            text: "Assalamu'alaikum Warrahmatullahi Wabarakatuh,\n",
+            delayAfter: 50
+        },
+        
+        {
+            text: "Happy Eid al-Fitr 1445 Hijri, my brothers/sisters.\n",
+            delayAfter: 50
+        },
+        
+        {
+            text: "I am Hassan. On this holy and blessed day, if I have ever done wrong either in behavior or in speech...\n",
+            delayAfter: 50
+        },
+        
+        {
+            text: "...then remember that it was all... INADVERTENTLY CAUSED BY ME. It was truly my mistake.\n",
+            delayAfter: 50
+        },
+        
+        {
+            text: "Therefore, on a personal note, I would like to extend my deepest apologies for all of my shortcomings.\n",
+            delayAfter: 50
+        },
+        
+        {
+            text: "Minal Aidzin Wal Faidzin.\n",
+            delayAfter: 50
+        },
+        
+        {
+            text: "Wassalamu'alaikum Warrahmatullahi Wabarakatuh.\n",
+            delayAfter: 50
+        },
+    ];
+
+    typewriter.playSequence(sequence);
+}
